0b73c430a481fbf2c8f5137bcbf58850e2f8c32f
trisyabana22 <cobainaja1111@gmail.com>
2022-04-07 14:20:22 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum adipisci sapiente explicabo aliquid quaerat dolor ullam eum, eligendi laboriosam reiciendis architecto deserunt nemo dicta? Eum tempore quae quaerat illum est?</p>
+    <ul> 
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
